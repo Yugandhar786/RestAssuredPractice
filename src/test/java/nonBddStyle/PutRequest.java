687aae52891dca9c86522c158add1b0c8a7800dc
YUGANDHAR V G <vgyugandhargopi2708@gmail.com>
2025-06-22 14:39:02 +0530

    Rest Assured Practice Put Method non BDD Style

---
+++ src/test/java/nonBddStyle/PutRequest.java
@@ -0,0 +1,28 @@
+package nonBddStyle;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class PutRequest {
+
+    public static void main(String[] args) {
+        RestAssured.baseURI="http://localhost:3000/";
+        RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json").body("{\n" +
+                "    \"FirstName\": \"Yugandhar\",\n" +
+                "    \"lastName\": \"V G1\",\n" +
+                "    \"UserName\": \"Yugandhar1\",\n" +
+                "    \"Password\": \"YUga@71\"\n" +
+                "  }");
+        Response response = requestSpecification.request(Method.PUT,"users/2");
+
+        System.out.println(response.getStatusLine());
+        System.out.println(response.statusCode());
+        System.out.println(response.asPrettyString());
+
+
+
+
+    }
+}
