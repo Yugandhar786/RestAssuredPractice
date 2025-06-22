de0584bc318bd9ecec428b77ebd5c3578c8d2253
YUGANDHAR V G <vgyugandhargopi2708@gmail.com>
2025-06-22 14:26:41 +0530

    Rest Assured Practice Post Method non BDD Style

---
+++ src/test/java/nonBddStyle/PostRequest.java
@@ -0,0 +1,28 @@
+package nonBddStyle;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class PostRequest {
+
+    public static void main(String[] args) {
+        RestAssured.baseURI="http://localhost:3000/";
+        RequestSpecification requestSpecification = RestAssured.given().body("{\n" +
+                "    \"FirstName\": \"Yugandhar\",\n" +
+                "    \"lastName\": \"V G1\",\n" +
+                "    \"UserName\": \"Yugandhar1\",\n" +
+                "    \"Password\": \"YUga@71\"\n" +
+                "  }");
+        Response response = requestSpecification.request(Method.POST,"users");
+
+        System.out.println(response.getStatusLine());
+        System.out.println(response.statusCode());
+        System.out.println(response.asPrettyString());
+
+
+
+
+    }
+}
